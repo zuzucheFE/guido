7c4fffb1e8ecf87a8d63ff143426f18cd079a185
kidney <kidneyleung@gmail.com>
2016-10-10 11:50:53 +0800

    更新image-dataurl测试用例

--- test/expect/image-dataurl/js/index.js
+++ test/expect/image-dataurl/js/index.js
@@ -48,7 +48,21 @@
 	
 	__webpack_require__(1);
 	
-	document.body.innerHTML = '<img src="' + __webpack_require__(2) + '">';
+	var fragment = document.createDocumentFragment();
+	
+	var div1 = document.createElement('div');
+	div1.innerHTML = '<img src="' + __webpack_require__(4) + '">';
+	fragment.appendChild(div1);
+	
+	var div2 = document.createElement('div');
+	div2.innerHTML = '<img src="' + __webpack_require__(3) + '">';
+	fragment.appendChild(div2);
+	
+	var div3 = document.createElement('div');
+	div3.innerHTML = '<img src="' + __webpack_require__(2) + '">';
+	fragment.appendChild(div3);
+	
+	document.body.appendChild(fragment);
 
 /***/ },
 /* 1 */
@@ -58,6 +72,18 @@
 
 /***/ },
 /* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__.p + "images/aircraft-232e884c.png";
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	module.exports = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAsCAMAAAAUyMtVAAAAUVBMVEVHcEwZsf/8NgYjrf8ZsP8asP8asP8ZsP8ZsP8Zsf8Zsf8Zsf8Zsf8Zsf8Zsf8Zsf8Zsf8Zsf8asf8asP8asP8Zsf8Zsf/7Ngb7NQX8NQUasf+A7QW+AAAAGnRSTlMA+vwFsA4Z4ybwu6LJYNpuinhBN07RlU7gXkieiXIAAAGCSURBVEjHrZXbkoMgEESHBRFFRPGSrP//oYsScCB4q9p+SgmnnG5aAkxxeKRlWZpePAOspvvM4tV2wzPAquzMM2BlZkMfAVaVHOkjwDHFNdC0mCFMFYcAcXtm0U8RU+cOdZtAux2SgtBNNFyteQ4AFQgAruuISYrgABjdWMzlw1VNMIOLIKzWt36I2jstFIuYUITXqvWHSQjLjLKKi7AyP1bvbd241QanSROmpvAOAAxubYpzoSZkXfUUv8H6yREiZCa35xgAUTqDO1F03nrzMf1r9YKEKH2KqvSGld+irdQ+AG8RMfhTJ10RHVyJRubOYiWAy5CNSE4aA4GYfZ7t+NWlCIAiqt8W5TkABSqf5Ll6p8C8JFGeAyHKZTr64jAwIAfsGtijvAXQPkTZ3wHGcG101FwDAoVZmUtgb+U2FZEXwN5K3d4wXeFWonNmV3erayWdbwJ7KzW5AUSt/FwgZ0DSStGeA9+t3KwfAaXKPF+t5wH8gUfSJAuw4z9oI+Ef9AftHUrU4iV78QAAAABJRU5ErkJggg=="
+
+/***/ },
+/* 4 */
 /***/ function(module, exports, __webpack_require__) {
 
 	module.exports = __webpack_require__.p + "images/buzz-9407c5eb.jpg";
--- test/fixtures/image-dataurl/src/js/index.js
+++ test/fixtures/image-dataurl/src/js/index.js
@@ -2,4 +2,18 @@
 
 require('../css/index.css');
 
-document.body.innerHTML = '<img src="' + require('../images/buzz.jpg') + '">';
+let fragment = document.createDocumentFragment();
+
+let div1 = document.createElement('div');
+div1.innerHTML = '<img src="' + require('../images/buzz.jpg') + '">';
+fragment.appendChild(div1);
+
+let div2 = document.createElement('div');
+div2.innerHTML = '<img src="' + require('../images/aircraft.png') + '">';
+fragment.appendChild(div2);
+
+let div3 = document.createElement('div');
+div3.innerHTML = '<img src="' + require('../images/aircraft.png?__url') + '">';
+fragment.appendChild(div3);
+
+document.body.appendChild(fragment);
