0e8697eb46d2f14961f941a8d78c640ff0543152
kidney <kidneyleung@gmail.com>
2016-12-15 00:51:39 +0800

    更新模板测试用例

---
+++ test/expect/template/js/simple-b4409316.js
@@ -0,0 +1,22 @@
+webpackJsonp([2,0],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	__webpack_require__(2);
+	
+	document.querySelector('.simple').innerHTML = 'i am simple page';
+	
+	console.log('i am simple page');
+
+/***/ },
+/* 1 */,
+/* 2 */
+/***/ function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ }
+]);
+//# sourceMappingURL=simple-b4409316.js.map
