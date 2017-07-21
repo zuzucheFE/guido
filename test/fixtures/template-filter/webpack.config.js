d4955ac123944fbce7c1e32b97bd90e5d7bf9651
kidney <kidneyleung@gmail.com>
2017-07-22 00:52:29 +0800

    bugfix: html options filter change test

--- test/fixtures/template-filter/webpack.config.js
+++ test/fixtures/template-filter/webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
         index: './src/js/index.js'
     },
     html: {
-        filter: function (path) {
+        test: function (path) {
             return /tpl(?:1|3)\.view\.handlebars/.test(path);
         }
     }
