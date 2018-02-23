2c8f41cbe49c5659d9bc243743c017490bbfe992
kidneyleung <kidneyleung@gmail.com>
2018-02-23 10:39:04 +0800

    test: 更新测试用例

--- test/fixtures/imagemin-jpg-min/webpack.config.js
+++ test/fixtures/imagemin-jpg-min/webpack.config.js
@@ -2,6 +2,9 @@ module.exports = {
     entry: {
         index: './src/js/index.js'
     },
+    output: {
+        filename: '[name].js'
+    },
     imagemin: {
         mozjpeg: {
             enabled: true,
