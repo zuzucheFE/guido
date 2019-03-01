fa2061e77e4fb24827f0d0ae1fcddf0f94fefab9
liangjingxian <liangjingxian@zuzuche.com>
2019-03-01 10:37:26 +0800

    docs: siteConfig头部链接配置

--- docusaurus/website/siteConfig.js
+++ docusaurus/website/siteConfig.js
@@ -38,10 +38,8 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
-    {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
+    {doc: 'getting-started', label: 'Docs'},
+    {href: 'https://github.com/zuzucheFE/guido', label: 'GitHub'},
   ],
 
   // If you have users set above, you add it here:
